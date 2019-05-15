b7b343eacea5b64d4d81d84d25634ddb7bb8a8d8
Khalil Haji <khalilalihaji@gmail.com>
2019-05-15 21:56:21 +0300

    added package installation to setup script

--- nokta.sh
+++ nokta.sh
@@ -9,18 +9,69 @@
 #                                         #
 ###########################################
 
+mkdir -pv "$HOME/Downloads"
+mkdir -pv "$HOME/Documents"
+mkdir -pv "$HOME/.config"
+
+
 stow -t ~ \
      bin \
      compton \
      dunst \
      emacs \
-     i3 \
      fish \
+     i3 \
      kitty \
+     oomox \
      polybar \
      rofi \
      vim \
      x
+
+
+
+# sudo pacman -S --needed --noconfirm reflector
+
+# sudo reflector --latest 15 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+sudo pacman -Syyu --needed --noconfirm base base-devel git stow
+
+
+function install {
+    PACKAGES="$PACKAGES ${@:2}"
+}
+
+
+install system os-prober
+install shell kitty fish bash-completion neofetch htop
+install networking networkmanager network-manager-applet curl wget
+install vpn nordvpn-bin
+install gui xorg xbindkeys xorg-xinit xdotool xdg-utils
+install wm i3-gaps polybar dunst compton betterlockscreen rofi feh maim
+install rust rustup
+install java openjdk8-doc openjdk8-src jdk8-openjdk
+install node npm
+install racket racket
+install python python python2 python-virtualenv python-pip python2-pip
+install themes oomox-git
+install fonts ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-font-awesome ttf-dejavu ttf-google-fonts-git noto-fonts-emoji powerline-fonts-git
+install drivers xf86-video-intel bumblebee
+install audio alsa-utils pulseaudio pavucontrol
+install music spotify playerctl cava
+install video mpv youtube-dl
+install torrent transmission-cli transmission-gtk
+install web google-chrome firefox
+install files thunar
+install communication slack-desktop discord riot-desktop
+install editor emacs
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd ..
+rm -rf yay
+
+yay -Sq --needed --noconfirm $PACKAGES
 
 # Export oomox configuration as gtk theme
 oomox-cli $HOME/.config/oomox/colors/Custom/gruvbox --output oomox-gruvbox
