fa16466e6d380f525ed6afd376131eaaca1c6a61
Khalil Haji <khalilalihaji@gmail.com>
2020-03-27 14:46:38 -0700

    Updates emacs configuration

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -9,6 +9,8 @@
 ;; activate package manager
 (package-initialize)
 
+(setq-default cursor-type 'bar)
+
 ;; initialize melpa
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'load-path "~/.emacs.d/lisp/")
@@ -185,6 +187,10 @@
 (use-package docker-compose-mode
   :ensure t)
 
+(use-package rjsx-mode
+  :ensure t
+  :config (add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode)))
+
 ;; Git stuff:
 (use-package magit
   :ensure t
@@ -209,7 +215,14 @@
 (use-package nov
   :ensure t)
 
-(global-visual-line-mode t)
+
+;; Set default font
+(set-face-attribute 'default nil
+                    :family "Source Code Pro"
+                    :height 110
+                    :weight 'normal
+                    :width 'normal)
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -221,7 +234,7 @@
     ("8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" default)))
  '(package-selected-packages
    (quote
-    (yasnippet-snippets yasnippet doom-themes web-mode docker-compose-mode docker dockerfile-mode vue-mode haskell-mode nov mu4e-alert mu4e scribble-mode rainbow-delimiters doom-modeline doom-city-lights-brighter-modeline neotree all-the-icons evil-magit evil-leader evil esup magit racer cargo rust-mode company smartparens org-bullets hacker-typer racket-mode gruvbox-theme counsel which-key try projectile use-package)))
+    (floobits floobits-emacs yasnippet-snippets yasnippet doom-themes web-mode docker-compose-mode docker dockerfile-mode vue-mode haskell-mode nov mu4e-alert mu4e scribble-mode rainbow-delimiters doom-modeline doom-city-lights-brighter-modeline neotree all-the-icons evil-magit evil-leader evil esup magit racer cargo rust-mode company smartparens org-bullets hacker-typer racket-mode gruvbox-theme counsel which-key try projectile use-package)))
  '(send-mail-function (quote smtpmail-send-it)))
 
 (custom-set-faces
